a564d5dcbf4b7b7e5fec4b4205a140c1e27a601e
KUNDAN <starkkumarkk1611@gmail.com>
2020-11-04 11:40:57 +0530

    one file added for same problem time complexcity of code is n

---
+++ optimised_time_complexcity_of_n.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int current_sum=0;
+    int mx=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        current_sum=current_sum+a[i];
+        if(a[i]<0)
+        {
+            current_sum=0;
+        }
+        
+        mx=max(mx,current_sum);
+    }
+    cout<<mx;
+    return 0;
+}
